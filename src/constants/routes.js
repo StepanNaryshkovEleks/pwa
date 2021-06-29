ed80060e7d1c4bc0982a9a498bb8969ec93f1a99
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-06-29 16:38:46 +0300

    init sign in page

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,6 +2,7 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sing-in",
   SIGN_UP: "/sign-up",
+  FORGOT_PASSWORD: "/forgot-password",
 };
 
 export default ROUTES;
--- src/pages/sign-in/component.js
+++ src/pages/sign-in/component.js
@@ -1,14 +1,47 @@
 import React from "react";
 import CNST from "../../constants";
 import {Link} from "react-router-dom";
-import {Switch} from "antd";
+import {Button, Form, Input} from "antd";
+import styles from "./_.module.css";
+import Header from "../../components/header";
+
+const formItemLayout = {
+  labelCol: {span: 24},
+  wrapperCol: {span: 24},
+};
 
 export const SignIn = () => {
   return (
     <>
-      <h1>Sign in page!!!!!!!</h1>
-      <Switch defaultChecked />
-      <Link to={CNST.ROUTES.SIGN_UP}>sign up</Link>
+      <Header title="Sign in" />
+      <div className={styles.wrap}>
+        <Form name="basic" {...formItemLayout}>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{required: true, message: "Please input your username!"}]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{required: true, message: "Please input your password!"}]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Link to={CNST.ROUTES.FORGOT_PASSWORD}>Forgot your Password?</Link>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Log In
+            </Button>
+          </Form.Item>
+        </Form>
+        <span>
+          I don’t have an account. <Link to={CNST.ROUTES.FORGOT_PASSWORD}>Create</Link>
+        </span>
+      </div>
     </>
   );
 };
