9c79ad6760fdf52d461dd2ad90660096cd0b3dae
MaryanaMysak <66512023+MaryanaMysak@users.noreply.github.com>
2021-07-06 10:01:28 +0300

    Feature/settings (#7)
    * added branch name rules
    
    * added fonts
    
    * added home page
    
    * add settings page
    
    Co-authored-by: StepanNaryshkov <stepan.naryshkov@eleks.com>

---
+++ src/pages/dashboard/component.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "../../components/header";
+import {Link} from "react-router-dom";
+import CNST from "../../constants";
+import settingsIcon from "../../images/settings.svg";
+
+const SettingsIcon = ({className}) => (
+  <Link to={CNST.ROUTES.SETTINGS}>
+    <img src={settingsIcon} alt="Settings" className={className} />
+  </Link>
+);
+
+const UserImage = ({userImg, className}) => (
+  <img src={userImg} alt="User" className={className} />
+);
+
+export const Dashboard = ({userImg}) => {
+  return (
+    <>
+      <Header
+        title="Welcome to Vee"
+        LeftComponent={SettingsIcon}
+        RightComponent={(props) => UserImage({...props, userImg})}
+      />
+    </>
+  );
+};
