72719ab42aa5611b0845c7bead26c6ccd81ed7c8
MaryanaMysak <66512023+MaryanaMysak@users.noreply.github.com>
2021-07-07 20:23:35 +0300

    Feature/search (#10)
    * added branch name rules
    
    * added fonts
    
    * added home page
    
    * add search bar
    
    * fix comment
    
    Co-authored-by: StepanNaryshkov <stepan.naryshkov@eleks.com>

---
+++ src/components/search/component.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Input} from "antd";
+import styles from "./_.module.css";
+import searchIcon from "../../images/search.svg";
+
+const Prefix = () => <img src={searchIcon} alt="Search" className={styles.prefix} />;
+
+export const Search = ({value, setValue}) => {
+  return (
+    <div className={styles.search}>
+      <Input
+        placeholder={"Search"}
+        prefix={<Prefix />}
+        allowClear={true}
+        value={value}
+        onChange={(ev) => setValue(ev.target.value)}
+        className={!value && styles.emptyInput}
+      />
+    </div>
+  );
+};
