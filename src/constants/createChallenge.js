d0144b3c7fa7ec485b1a5ed68376aa737b39709b
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-07-09 13:02:44 +0300

    deleted consts

--- src/constants/createChallenge.js
+++
@@ -1,8 +0,0 @@
-const CREATE_CHALLENGE = {
-  DETAILS: {
-    SET_NAME: "SET_NAME",
-    SET_DESC: "SET_DESC",
-  },
-};
-
-export default CREATE_CHALLENGE;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,13 +1,11 @@
 import user from "./user";
 import routes from "./routes";
 import signUpDetails from "./signUpDetails";
-import createChallenge from "./createChallenge";
 
 const CNST = {
   USER: user,
   ROUTES: routes,
   SIGN_UP_DETAILS: signUpDetails,
-  CREATE_CHALLENGE: createChallenge,
 };
 
 export default CNST;
