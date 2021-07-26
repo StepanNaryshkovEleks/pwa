c22f7c804cd9d71ae02a3304a39c7c7a51a20ec7
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-07-26 19:15:40 +0300

    fix url for axios

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://api.bucha.click:35353";
+axios.defaults.baseURL = "https://api.bucha.click:35353";
 
 // axios.defaults.baseURL = "https://bucha.click:35353";
-axios.defaults.baseURL = "http://localhost:8010/proxy";
+// axios.defaults.baseURL = "http://localhost:8010/proxy";
