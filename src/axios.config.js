193ac62fc07b688ba67d5e4882d7e7a6b40d1183
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-08-02 08:59:07 +0300

    fixed sign up url

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.bucha.click:35353";
+// axios.defaults.baseURL = "https://api.bucha.click:35353";
 
 // axios.defaults.baseURL = "https://bucha.click:35353";
-// axios.defaults.baseURL = "http://localhost:8010/proxy";
+//axios.defaults.baseURL = "http://localhost:8010/proxy";
+
+axios.defaults.baseURL =
+  window.location.hostname !== "localhost"
+    ? "https://api.bucha.click:35353"
+    : "http://localhost:8010/proxy";
