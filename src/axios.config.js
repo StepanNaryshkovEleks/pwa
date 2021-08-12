c8834e145933ff95bd8bb96d6a2369471c66932f
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-08-12 11:55:47 +0300

    changed url

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
-axios.defaults.headers["Access-Control-Allow-Methods"] =
-  "GET, PUT, POST, DELETE, OPTIONS";
+// axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+// axios.defaults.headers["Access-Control-Allow-Methods"] =
+//   "GET, PUT, POST, DELETE, OPTIONS";
 axios.defaults.baseURL =
   window.location.hostname !== "localhost"
-    ? "https://api.bucha.click:37189"
+    ? "https://api.bucha.click:42911"
     : "http://localhost:8010/proxy";
