c15f2daec4cfaa3ffff567404ec039ef5a86d726
StepanNaryshkovEleks <82516098+StepanNaryshkovEleks@users.noreply.github.com>
2021-08-26 10:24:57 +0300

    added rules for files (#43)

---
+++ src/constants/allowedFiles.js
@@ -0,0 +1,14 @@
+export default [
+  ".jpeg",
+  ".jpg",
+  ".png",
+  ".gif",
+  ".bmp",
+  ".heic",
+  ".heif",
+  ".mp4",
+  ".webm",
+  ".m4v",
+  ".mov",
+  ".qt",
+];
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,12 +2,14 @@ import user from "./user";
 import routes from "./routes";
 import signUpDetails from "./signUpDetails";
 import challenge from "./challenge";
+import allowedFiles from "./allowedFiles";
 
 const CNST = {
   USER: user,
   ROUTES: routes,
   SIGN_UP_DETAILS: signUpDetails,
   CHALLENGE: challenge,
+  ALLOWED_FILES: allowedFiles,
 };
 
 export default CNST;
