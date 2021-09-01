7164c232aa98b98fc6b596c03a5cc2e87a013b7c
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-09-01 13:57:59 +0300

    added new url

--- src/axios.config.js
+++ src/axios.config.js
@@ -3,4 +3,4 @@ import axios from "axios";
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers["Access-Control-Allow-Methods"] =
   "GET, PUT, POST, DELETE, OPTIONS";
-axios.defaults.baseURL = "https://api.bucha.click:45709";
+axios.defaults.baseURL = "https://api.bucha.click:42539";
