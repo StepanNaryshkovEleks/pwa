2415a6371af314faa7b780ee10e96562efe6a8d0
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-09-07 17:50:12 +0300

    added test promt from mdn

--- src/App.js
+++ src/App.js
@@ -7,9 +7,47 @@ import Spinner from "./components/spinner";
 function App({initUser, fetching}) {
   useEffect(() => {
     initUser();
+
+    let deferredPrompt;
+    const addBtn = document.querySelector(".add-button");
+
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      addBtn.style.display = "block";
+
+      addBtn.addEventListener("click", (e) => {
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = "none";
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("User accepted the A2HS prompt");
+          } else {
+            console.log("User dismissed the A2HS prompt");
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
   }, [initUser]);
   return (
     <>
+      <button
+        className="add-button"
+        style={{
+          display: "none",
+          position: "absolute",
+          zIndex: 100,
+        }}
+      >
+        Add to home screen
+      </button>
       {fetching && <Spinner />}
       <AppRouter />
     </>
