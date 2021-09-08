8b41cdaf25477430c3ac0baf7cf023d75ac6451f
StepanNaryshkovEleks <82516098+StepanNaryshkovEleks@users.noreply.github.com>
2021-09-08 12:00:31 +0300

    handling the same username (#57)

--- src/helpers/axios-interceptors/response.js
+++ src/helpers/axios-interceptors/response.js
@@ -3,10 +3,11 @@ import {notification} from "antd";
 import {signOutAction} from "../../redux/actions/user/signOut";
 import {store} from "./../../index";
 
-const openNotificationWithIcon = (code) => {
+const openNotificationWithIcon = (code, text) => {
+  const description = text ? text : "Something went wrong, " + code + " error.";
   notification["error"]({
     message: "Error",
-    description: "Something went wrong, " + code + " error.",
+    description,
   });
 };
 
@@ -18,8 +19,16 @@ axios.interceptors.response.use(
     }
 
     if (error.response.status >= 400) {
-      if (error.response.status === 403) {
+      if (
+        error.response.status === 403 &&
+        error.response.config.url !== "application/vee/signup"
+      ) {
         store.dispatch(signOutAction());
+      } else if (
+        error.response.status === 403 &&
+        error.response.config.url === "application/vee/signup"
+      ) {
+        openNotificationWithIcon(403, "This name already exists");
       } else {
         openNotificationWithIcon(error.response.status);
       }
