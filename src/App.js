d95600e647170c453b6de2efdac842a38275bede
StepanNaryshkovEleks <82516098+StepanNaryshkovEleks@users.noreply.github.com>
2021-09-09 16:54:53 +0300

    added no internet connection popup (#63)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import AppRouter from "./routes";
 import {initUserAction} from "./redux/actions/user/initUser";
 import {connect} from "react-redux";
 import Spinner from "./components/spinner";
+import {Button, notification} from "antd";
+
+const openNotification = () => {
+  notification.warning({
+    className: "no-internet-popup",
+    message: `No internet connection. Please try again later`,
+    description: (
+      <Button type="link" onClick={() => document.location.reload()}>
+        Refresh page
+      </Button>
+    ),
+    placement: "topRight",
+  });
+};
 
 function App({initUser, fetching}) {
+  useEffect(() => {
+    window.addEventListener("offline", () => openNotification());
+  }, []);
+
   useEffect(() => {
     initUser();
   }, [initUser]);
