3066f917ba6928f9fff0af0828cd71b57059e5e6
StepanNaryshkov <stepan.naryshkov@eleks.com>
2022-06-12 20:12:11 +0300

    updated url

--- src/axios.config.js
+++ src/axios.config.js
@@ -3,4 +3,5 @@ import axios from "axios";
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers["Access-Control-Allow-Methods"] =
   "GET, PUT, POST, DELETE, OPTIONS";
+
 axios.defaults.baseURL = "https://api.vee.org.uk:41437";
